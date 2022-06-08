cc96fa15da249c1a0c9d9804faf21aab4200816e
Telegram1999 <106989715+Telegram1999@users.noreply.github.com>
2022-06-08 23:18:33 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,27 +4,27 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/Ak_Bot_SupportGroup>Team AkBotDevs</a>
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=http://instagram.com/kl_02_pranav_?utm_source=qr>kmtz</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     ADMINS_TXT = """Hey Bruh,
-My name is LUCIFER
+My name is ROLEX
 - My Admins are:
-- @IAM_A_JOKER
-  @JINN_SULTHAN
-  @psychopath_here 
+- @KL_2335
+  @kmtz_channel_v3
+  @Rolex2335 
 
 <b>DEVS:</b>
-- <a href=https://t.me/Ak_Bot_SupportGroup>Team AkBotDevs</a>"""
+- <a href=https://t.me/kmtz_channel_v3>Team kmtz</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and LUCIFER will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. LUCIFER should have admin privillage.
+1. Rolex should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
