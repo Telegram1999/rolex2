9ecbd897a4489ca91db5f68d9186e87122b012c7
Telegram1999 <106989715+Telegram1999@users.noreply.github.com>
2022-06-08 23:38:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -31,7 +31,7 @@ async def start(client, message):
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
-        await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
+        await asyncio.sleep(2) # 😢 https://github.com/Telegram1999/rolex2 😬 wait a bit, before checking.
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
             await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
@@ -44,13 +44,13 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🤖 Creators', url='https://t.me/Ak_Bot_SupportGroup'),
+            InlineKeyboardButton('🤖 Creators', url='https://t.me/kmtz_channel_v3'),
             InlineKeyboardButton('😊 About', callback_data='about')
             ],[
-            InlineKeyboardButton('😎 YOUTUBE', url='https://youtube.com/c/MADpsychoYT'),
+            InlineKeyboardButton('😎 YOUTUBE', url='http://instagram.com/kl_02_pranav_?utm_source=qr'),
             InlineKeyboardButton('ℹ️ Help', callback_data='help')
             ],[
-            InlineKeyboardButton('❤️ 𝓢𝓸𝓾𝓻𝓬𝓮', url='https://github.com/Akbotmakersnew/LUCIFER')
+            InlineKeyboardButton('❤️ 𝓢𝓸𝓾𝓻𝓬𝓮', url='https://github.com/Telegram1999/rolex2')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
